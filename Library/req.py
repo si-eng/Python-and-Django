a009fcee230da96996a2f47b478992f76f831551
Sakshi Sharma <SharmaSakshi@SEASIAINFOTECH.COM>
2024-01-15 17:33:46 +0530

    pythonbasics

--- Library/req.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Making an HTTP GET request
-response = requests.get("https://www.example.com")
-
-# Printing the status code and content
-print("Status Code:", response.status_code)
-print("Content:")
-print(response.text)
---
+++ mypractice/04_osfile.py
@@ -0,0 +1,7 @@
+student_score = {   'Ritika': 5,
+                    'Sam': 7, 
+                    'John': 10, 
+                    'Aadi': 8}
+# Iterate over key/value pairs in dict and print them
+for key, value in student_score.items():
+    print(key, ' : ', value)
