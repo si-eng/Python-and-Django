72cb9d83422b4109673f4815974fda3e344c6475
Sakshi Sharma <SharmaSakshi@SEASIAINFOTECH.COM>
2024-01-16 17:49:21 +0530

    lib&Operators

---
+++ Library/Pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Creating a Pandas DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 22]}
+
+df = pd.DataFrame(data)
+
+# Displaying the DataFrame
+print("DataFrame:")
+print(df)
